81469083cbea29a5dea7f854b1d149eae7752b80
egaalfy <egaalifyaf@gmail.com>
2022-01-19 09:58:31 +0700

    fix error authorization 401

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,44 @@
 <script>
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+// kebutuhan redirect login
+import { useStore } from 'vuex'; // store Vuex
+import Api from './api/Api';
+import { useRouter } from 'vue-router';
 
 export default {
     components: {
         Header,
         Footer,
+    },
+
+    // trick agar redirect ke login saat error 401
+    setup() {
+        const router = useRouter();
+        const store = useStore();
+        const token = localStorage.getItem('token');
+        // console.log(token);
+        // console.log(token);
+        Api.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+        Api.get('/user')
+            .then((response) => {
+                //commit ke mutatuin GET_USER dengan hasil response
+                console.log(response);
+            })
+            .catch((error) => {
+                if (error.response.data.message === 'Unauthenticated.') {
+                    localStorage.removeItem('token');
+                    localStorage.removeItem('user');
+                    if (token === null) {
+                        return router.push({ name: 'login' });
+                    }
+                } else {
+                    console.log(error.response.data.message);
+                }
+            });
+        return {
+            store,
+        };
     },
 };
 </script>
